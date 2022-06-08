d773664fbf5c1d2e649091206070973b141567e6
dhruviex <C0846368@mylambton.ca>
2022-06-08 16:03:07 -0400

    Added polyline and polygon
    updated the code with polyline and polygon

--- app/src/main/java/com/example/lab1_dhruvbakshi_c0846368_android/MapsActivity.java
+++ app/src/main/java/com/example/lab1_dhruvbakshi_c0846368_android/MapsActivity.java
@@ -8,12 +8,18 @@ import androidx.fragment.app.FragmentActivity;
 
 import android.Manifest;
 import android.content.pm.PackageManager;
+import android.graphics.Color;
 import android.location.Address;
 import android.location.Geocoder;
+import android.net.Uri;
 import android.nfc.Tag;
 import android.os.Bundle;
 import android.util.Log;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+import com.google.android.gms.common.api.Response;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
@@ -23,10 +29,17 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 import com.example.lab1_dhruvbakshi_c0846368_android.databinding.ActivityMapsBinding;
+import com.google.android.gms.maps.model.Polygon;
+import com.google.android.gms.maps.model.PolygonOptions;
+import com.google.android.gms.maps.model.Polyline;
+import com.google.android.gms.maps.model.PolylineOptions;
 import com.google.android.material.shape.MarkerEdgeTreatment;
 
+
 import java.io.IOException;
+import java.net.URL;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback , GoogleMap.OnMapLongClickListener  , GoogleMap.OnMarkerDragListener{
@@ -34,6 +47,9 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     private static final String TAG = "MapsActivity";
     private GoogleMap mMap;
     private static final int LOCATION_REQUEST = 500;
+    Polyline polyline = null;
+    Polygon polygon = null;
+
     ArrayList<LatLng> listPoints;
 
     private ActivityMapsBinding binding;
@@ -120,7 +136,10 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
 
         mMap.addMarker(new MarkerOptions().position(latLng));
+        mMap.addPolyline(new PolylineOptions().add(latLng));
+
     }
+
 
     @Override
     public void onMarkerDrag(@NonNull Marker marker) {
@@ -137,6 +156,22 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 Address address = addresses.get(0);
                 String streetAddress = address.getAddressLine(0);
                 marker.setTitle(streetAddress);
+                //Draw polyline on Map
+                if (polyline != null) polyline.remove();
+                //Create Polyline Options
+                PolylineOptions polylineOptions = new PolylineOptions()
+                        .addAll(Collections.singleton(latLng)).clickable(true);
+                polyline = mMap.addPolyline(polylineOptions);
+                //Draw Polygon On map
+                if (polygon != null ) polygon.remove();
+                //Create polygonOptions
+                PolygonOptions polygonOptions = new PolygonOptions().addAll(Collections.singleton(latLng))
+                        .clickable(true);
+                polygon = mMap.addPolygon(polygonOptions);
+                //Set Polygon Stroke Color
+                polygon.setStrokeColor(Color.green(6));
+
+
             }
         } catch (IOException e) {
             e.printStackTrace();
@@ -147,4 +182,37 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
     public void onMarkerDragStart(@NonNull Marker marker) {
         Log.d(TAG , "onMarkerDragStart:");
     }
+
+
+   /* private List<LatLng> decodePoly(String encoded){
+        List<LatLng> poly = new ArrayList<>();
+        int index = 0 , len = encoded.length();
+        int lat  = 0 , lng  = 0;
+
+        while (index < len ) {
+            int b , shift = 0, result = 0;
+            do{
+                b = encoded.charAt(index++) - 63;
+                result |= (b & 0x1f) << shift;
+                shift += 5 ;
+            }while (b > 0x20);
+            int dlat = ((result & 1) == 0 ? ~(result >> 1) : (result >> 1));
+           lat += dlat;
+
+           shift = 0;
+           result = 0;
+           do{
+               b = encoded.charAt(index++) - 63;
+               result |= ((b & 0x1f)<< shift);
+               shift += 5;
+           }while (b> 0x20);
+           int dlng = ((result &1 ) != 0 ? ~(result >> 1) : (result >> 1));
+           lng += dlng;
+           LatLng p = new LatLng((((double)lat /1E5)),
+            (((double) lng /1E5)));
+           poly.add(p);
+
+        }
+        return poly;
+    }*/
 }
